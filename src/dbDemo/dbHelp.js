6f53fdc1f2c2eb6b4d809a8a9d4d62cf7c070318
Pamela Iman <pamelariman@gmail.com>
2024-06-06 14:42:02 +0100

    database connection from .env

--- src/dbDemo/dbHelp.js
+++ src/dbDemo/dbHelp.js
@@ -1,4 +1,6 @@
 const { Pool } = require("pg");
+const dotenv = require("dotenv");
+dotenv.config();
 
 /**
  *Makes a pool of connections to the named database.  it is assumed the db is on localhost.
@@ -6,8 +8,9 @@ const { Pool } = require("pg");
  */
 function makeDBConnectionPool(dbName) {
   //Understanding the details is not important here.
+  
   return new Pool({
-    database: dbName,
+    connectionString: process.env.DATABASE_URL
   });
 }
 
